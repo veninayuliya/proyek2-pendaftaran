019fa0ed223c6228d6db6f2d6ea3ebc2ff9ad5b1
shafa19 <nameless.shaff@gmail.com>
2021-11-19 22:01:46 +0700

    rev penerimaan

--- page/penerimaan.php
+++ page/penerimaan.php
@@ -49,15 +49,47 @@
 
 
     <div class="container-fluid">
-        <div class="col-lg-10"> <br>
+        <div class="col-lg-10">
+        <h3 class="my-4" align="center">Data Siswa</h3>
             <div class="card body">
-                <div class="container" style="margin:30px;text-align:justify;display:grid;">
-                    <img src="../img/<?php echo $d['foto'];?>" style="width:180px; height:250px;">
-                    <p><b>Nama</b>: <?php echo $d['nama_lengkap']; ?></p>
-                    <p><b>Jenis Kelamin</b>: <?php echo $d['jenis_kelamin']; ?></p>
-                    <p><b>Asal Sekolah</b>: <?php echo $d['sekolah_asal']; ?></p>
+                <div class="container" style="margin:30px;display:grid;">
+                    <div class="row">
+                        <div class="column" style="width:50%;padding:10px;">
+                            <h4><u>IDENTITAS SISWA</u></h4>
+                            <p><b>Nama Lengkap</b>: <?php echo $d['nama_lengkap']; ?></p>
+                            <p><b>Jenis Kelamin</b>: <?php echo $d['jenis_kelamin']; ?></p>
+                            <p><b>Tempat, Tanggal Lahir</b>: <?php echo $d['ttl']; ?></p>
+                            <p><b>Nomor Induk Kependudukan</b>: <?php echo $d['nik']; ?></p>
+                            <p><b>Nomor Akta Lahir</b>: <?php echo $d['no_akta_lahir']; ?></p>
+                            <p><b>Agama</b>: <?php echo $d['agama']; ?></p>
+                            <p><b>Asal Sekolah</b>: <?php echo $d['sekolah_asal']; ?></p>
+                            <p><b>Alamat Rumah</b>: <?php echo $d['alamat']; ?></p>
+                            <p><b>Nomor Telepon</b>: <?php echo $d['telepon']; ?></p><br>
+
+                            <h4><u>DATA PERIODIK</u></h4>
+                            <p><b>Tinggi Badan</b>: <?php echo $d['tb_siswa']; ?> cm</p>
+                            <p><b>Berat Badan</b>: <?php echo $d['bb_siswa']; ?> kg</p>
+                            <p><b>Jumlah Saudara</b>: <?php echo $d['jml_saudara']; ?></p>
+                            <p><b>Jarak Rumah ke Sekolah</b>: <?php echo $d['jarak']; ?> km</p><br>
+
+                            <h4><u>DATA AYAH KANDUNG</u></h4>
+                            <p><b>Nama Ayah</b>: <?php echo $d['nama_ayah']; ?></p>
+                            <p><b>NIK Ayah</b>: <?php echo $d['nik_ayah']; ?></p>
+                            <p><b>Pekerjaan Ayah</b>: <?php echo $d['pekerjaan_ayah']; ?></p>
+                            <p><b>Pendidikan Ayah</b>: <?php echo $d['pend_ayah']; ?></p><br>
+
+                            <h4><u>DATA IBU KANDUNG</u></h4>
+                            <p><b>Nama Ibu</b>: <?php echo $d['nama_ibu']; ?></p>
+                            <p><b>NIK Ibu</b>: <?php echo $d['nik_ibu']; ?></p>
+                            <p><b>Pekerjaan Ibu</b>: <?php echo $d['pekerjaan_ibu']; ?></p>
+                            <p><b>Pendidikan Ibu</b>: <?php echo $d['pend_ibu']; ?></p>
+                        </div>
+                        <div class="column" style="width:400px;padding:10px;">
+                            <img src="../img/<?php echo $d['foto'];?>" style="width:180px;height:250px;" align="right">
+                        </div>
+                    </div>
                 </div>
-            </div><br>
+            </div>
             <div class="alert alert-info">
                     <p>Status&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: <b><?php echo $d['status']; ?></b></p>
             </div>
